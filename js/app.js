cfe148443ec9bac81c675ed70b0bf461f981074e
Vitalii Chaikovskyi <garber1329@gmail.com>
2022-03-09 22:47:30 +0200

    fixed mobile touch

--- js/app.js
+++ js/app.js
@@ -4,10 +4,12 @@ const colors = document.getElementsByClassName('jsColor');
 const range = document.getElementById('jsRange');
 const mode = document.getElementById('jsMode');
 const saveBtn = document.getElementById('jsSave');
+const clear = document.getElementById('jsClear');
 
 const INITIAL_COLOR = '#2c2c2c';
 let CANVAS_WIDTH = window.innerWidth;
-let CANVAS_HEIGHT = (window.innerHeight*80)/100;
+let CANVAS_HEIGHT = (window.innerHeight * 80) / 100;
+let line_strong;
 
 console.log(CANVAS_WIDTH,CANVAS_HEIGHT)
 
@@ -59,11 +61,17 @@ if (canvas) {
 	canvas.addEventListener('contextmenu', function (event) {
 		event.preventDefault();
 	});
+
+
+	canvas.addEventListener("touchstart", handleStart, false);
+	canvas.addEventListener("touchend", handleEnd, false);
+	canvas.addEventListener("touchcancel", handleCancel, false);
+	canvas.addEventListener("touchmove", handleMove, false);
 }
 
 if (range) {
 	range.addEventListener('input', function (event) {
-		ctx.lineWidth = event.target.value;
+		ctx.lineWidth = line_strong = event.target.value;
 	})
 }
 
@@ -96,3 +104,101 @@ if (saveBtn) {
 	})
 }
 
+if (clear) {
+	clear.addEventListener('click', function () {
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+	})
+}
+
+var ongoingTouches = [];
+
+function handleStart(evt) {
+  evt.preventDefault();
+  console.log("touchstart.");
+  
+  var touches = evt.changedTouches;
+
+  for (var i = 0; i < touches.length; i++) {
+    console.log("touchstart:" + i + "...");
+    ongoingTouches.push(copyTouch(touches[i]));
+    ctx.beginPath();
+    ctx.arc(touches[i].pageX, touches[i].pageY, false);  // a circle at the start ctx.arc(touches[i].pageX, touches[i].pageY, 4, 0, 2 * Math.PI, false); 
+    ctx.fill();
+    console.log("touchstart:" + i + ".");
+  }
+}
+
+function handleMove(evt) {
+  evt.preventDefault();
+  
+  var touches = evt.changedTouches;
+
+  for (var i = 0; i < touches.length; i++) {
+    var idx = ongoingTouchIndexById(touches[i].identifier);
+
+    if (idx >= 0) {
+      console.log("continuing touch "+idx);
+      ctx.beginPath();
+      console.log("ctx.moveTo(" + ongoingTouches[idx].pageX + ", " + ongoingTouches[idx].pageY + ");");
+      ctx.moveTo(ongoingTouches[idx].pageX, ongoingTouches[idx].pageY);
+      console.log("ctx.lineTo(" + touches[i].pageX + ", " + touches[i].pageY + ");");
+      ctx.lineTo(touches[i].pageX, touches[i].pageY);
+      ctx.stroke();
+
+      ongoingTouches.splice(idx, 1, copyTouch(touches[i]));  // swap in the new touch record
+      console.log(".");
+    } else {
+      console.log("can't figure out which touch to continue");
+    }
+  }
+}
+
+function handleEnd(evt) {
+  evt.preventDefault();
+  console.log("touchend");
+  
+  var touches = evt.changedTouches;
+
+  for (var i = 0; i < touches.length; i++) {
+    var idx = ongoingTouchIndexById(touches[i].identifier);
+
+    if (idx >= 0) {
+      
+      ctx.beginPath();
+      ctx.moveTo(ongoingTouches[idx].pageX, ongoingTouches[idx].pageY);
+      ctx.lineTo(touches[i].pageX, touches[i].pageY);
+      //ctx.fillRect(touches[i].pageX - 4, touches[i].pageY - 4, 8, 8);  // and a square at the end
+      ongoingTouches.splice(idx, 1);  // remove it; we're done
+    } else {
+      console.log("can't figure out which touch to end");
+    }
+  }
+}
+
+function handleCancel(evt) {
+  evt.preventDefault();
+  console.log("touchcancel.");
+  var touches = evt.changedTouches;
+
+  for (var i = 0; i < touches.length; i++) {
+    var idx = ongoingTouchIndexById(touches[i].identifier);
+    ongoingTouches.splice(idx, 1);  // remove it; we're done
+  }
+}
+
+
+
+function copyTouch({ identifier, pageX, pageY }) {
+  return { identifier, pageX, pageY };
+}
+
+function ongoingTouchIndexById(idToFind) {
+  for (var i = 0; i < ongoingTouches.length; i++) {
+    var id = ongoingTouches[i].identifier;
+
+    if (id == idToFind) {
+      return i;
+    }
+  }
+  return -1;    // not found
+}
